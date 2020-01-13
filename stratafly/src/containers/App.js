29fc92198a2aa1b7114e2ffccb0a56785443a6f4
Elias Norrby <elias.norrby@gmail.com>
2020-01-13 08:02:44 +0100

    L0303: Set up Theme

--- stratafly/src/containers/App.js
+++ stratafly/src/containers/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react"
+
+import { MuiThemeProvider } from "@material-ui/core/styles"
+import { Theme } from "../Theme"
+
 import { Desktop } from "../components/Desktop"
+import { AppHeader } from "../components/AppHeader"
 
 class App extends Component {
   render() {
-    return <Desktop />
+    if (window.screen.width >= 1024 && window.screen.height >= 768)
+      return <Desktop />
+
+    return (
+      <MuiThemeProvider theme={Theme}>
+        <AppHeader />
+      </MuiThemeProvider>
+    )
   }
 }
 export default App
