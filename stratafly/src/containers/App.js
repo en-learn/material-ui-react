6f9e6f47e760b16d9e4f6dbd462a05d35f0b5325
Elias Norrby <elias.norrby@gmail.com>
2020-01-13 07:45:40 +0100

    L0: Setup

---
+++ stratafly/src/containers/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react"
+import { Desktop } from "../components/Desktop"
+
+class App extends Component {
+  render() {
+    return <Desktop />
+  }
+}
+export default App
